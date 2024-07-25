367f2dd93f65ce20008c25f7310840c83d411883
Sahil Kavatkar <sahilkavatkar@gmail.com>
2024-07-25 17:32:27 +0530

    Update EJS templates and CSS styles

--- public/js/loader.js
+++
@@ -1,7 +0,0 @@
-var loader = document.getElementById("loader");
-
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        loader.style.display = "none";
-    }, 2000);  // Adjust the delay time in milliseconds (1000ms = 1 second)
-});
